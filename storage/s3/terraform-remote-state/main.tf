82f146cfd78b233cdba44cfb81c4371f3995218d
Heechan Kim <heechan.dev@gmail.com>
2022-12-08 01:05:15 +0900

    Add bucket to public access block acl

--- storage/s3/terraform-remote-state/main.tf
+++ storage/s3/terraform-remote-state/main.tf
@@ -7,6 +7,12 @@ terraform {
       version = "~> 4.45"
     }
   }
+
+  # backend "s3" {
+  #   bucket = "skillup-terraform-remote-state"
+  #   key    = "storage/s3/terraform-remote-state/terraform.tfstate"
+  #   region = "ap-northeast-2"
+  # }
 }
 
 provider "aws" {
@@ -23,9 +29,25 @@ resource "aws_s3_bucket_acl" "this" {
   acl    = "private"
 }
 
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  block_public_acls   = true
+  block_public_policy = true
+  ignore_public_acls  = true
+}
+
 resource "aws_s3_bucket_versioning" "this" {
   bucket = aws_s3_bucket.this.id
   versioning_configuration {
     status = "Disabled"
   }
 }
+
+# data "aws_iam_policy_document" "full_access_to_devops" {
+#   statement {
+#     principals {
+#       type = ""
+#     }
+#   }
+# }
