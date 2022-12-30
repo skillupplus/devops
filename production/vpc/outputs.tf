e0400265b9ee58ccd4e50410550663767ed6aacb
Heechan Kim <heechan.dev@gmail.com>
2022-12-30 23:56:52 +0900

    Update outputs

--- production/vpc/outputs.tf
+++ production/vpc/outputs.tf
@@ -2,3 +2,10 @@ output "vpc_id" {
   value = aws_vpc.this.id
 }
 
+output "public_subnet_ids" {
+  value = [for subnet in aws_subnet.public : subnet.id]
+}
+
+output "private_subnet_ids" {
+  value = [for subnet in aws_subnet.private : subnet.id]
+}
