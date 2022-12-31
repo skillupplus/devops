d7707102eb91b2fa90976c79a5cd5fa98587041f
Heechan Kim <heechan.dev@gmail.com>
2023-01-01 01:36:11 +0900

    Update vpc

--- production/vpc/nat-instance.tf
+++ production/vpc/nat-instance.tf
@@ -19,11 +19,22 @@ locals {
 # NAT Instance
 ################################################################################
 
+data "aws_iam_role" "ec2-ssm" {
+  name = "ec2-ssm"
+}
+
+resource "aws_iam_instance_profile" "nat_instance" {
+  name = "${var.name}-production-nat-instance-profile"
+  role = data.aws_iam_role.ec2-ssm.id
+}
+
 resource "aws_instance" "nat_instance" {
   ami               = data.aws_ami.nat_instance.id
   instance_type     = "t3.nano"
   availability_zone = local.nat_instance_az
   subnet_id         = aws_subnet.public[local.nat_instance_az].id
+
+  iam_instance_profile = aws_iam_instance_profile.nat_instance.id
 
   vpc_security_group_ids = [
     local.default_sg_id
@@ -38,5 +49,6 @@ resource "aws_instance" "nat_instance" {
 }
 
 locals {
-  nat_instance_id = aws_instance.nat_instance.id
+  nat_instance_id     = aws_instance.nat_instance.id
+  nat_instance_eni_id = aws_instance.nat_instance.primary_network_interface_id
 }
--- production/vpc/vpc.tf
+++ production/vpc/vpc.tf
@@ -128,8 +128,8 @@ resource "aws_route_table" "private" {
   vpc_id = local.vpc_id
 
   route {
-    cidr_block  = "0.0.0.0/0"
-    instance_id = local.nat_instance_id
+    cidr_block           = "0.0.0.0/0"
+    network_interface_id = local.nat_instance_eni_id
   }
 
   tags = {
