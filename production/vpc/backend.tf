7df43ae31c3c43dac413bc60b5041471e4be7dfb
Heechan Kim <heechan.dev@gmail.com>
2022-12-31 20:53:55 +0900

    Move provider and version info into backend.tf

--- production/vpc/backend.tf
+++ production/vpc/backend.tf
@@ -1,8 +1,22 @@
 terraform {
+  required_version = ">= 1.3.6"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.45"
+    }
+  }
+
   backend "s3" {
     bucket  = "skillup-terraform-remote-state"
     key     = "production/vpc/terraform.state"
     region  = "ap-northeast-2"
     profile = "skillup"
   }
+}
+
+provider "aws" {
+  profile = "skillup"
+  region  = "ap-northeast-2"
 }
